122fb59fb814f36294e456608b407fca32872bf8
Byeongchan99 <harrrypoter@naver.com>
2024-02-14 05:34:30 +0900

    네비메쉬 테스트

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player; // 플레이어의 Transform
+    public Vector3 offset; // 플레이어와 카메라 사이의 오프셋(거리)
+
+    void LateUpdate()
+    {
+        // 카메라의 위치를 플레이어의 위치 + 오프셋으로 설정
+        transform.position = player.position + offset;
+    }
+}
