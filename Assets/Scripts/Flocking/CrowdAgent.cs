07dd28a82eed353d38717006d4c5e343510c8f4e
Lee Byoung Chan <harrrypoter@naver.com>
2024-09-27 20:27:32 +0900

    NavMesh 군중 처리

---
+++ Assets/Scripts/Flocking/CrowdAgent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CrowdAgent : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = Random.Range(4.0f, 5.0f);
+        agent.SetDestination(target.position);
+    }
+}
